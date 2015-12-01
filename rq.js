3663fec072df1267fb3ce3500a847b2f2eac6377
yaramdotnet <yaramdotnet@outlook.com>
2015-12-01 18:13:02 +0530

    First Commit

---
+++ rq.js
@@ -0,0 +1,20 @@
+﻿var quotes = {
+ "quotes_list":[
+        {
+        "quote": "Science doesn't purvey absolute truth. Science is a mechanism. It's a way of trying to improve your knowledge of nature. It's a system for testing your thoughts against the universe and seeing whether they match. And this works, not just for the ordinary aspects of science, but for all of life. I should think people would want to know that what they know is truly what the universe is like, or at least as close as they can get to it.",
+        "author": "Isaac Asimov"
+},
+{
+    "quote": "Those people who think they know everything are a great annoyance to those of us who do.",
+    "author": "Isaac Asimov"
+},
+{
+    "quote": "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    "author": "Albert Einstein"
+},
+{
+    "quote": "The important thing is not to stop questioning. Curiosity has its own reason for existing.",
+    "author": "Albert Einstein"
+},
+]
+}
